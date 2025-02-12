36a03393bb8dbcb0b6354d9fd2924e0b924afa63
chandan chanchal <chandanabbupur@gmail.com>
2025-02-12 15:02:12 +0530

    d

---
+++ src/test/java/com/example/webapp/HelloServletTest.java
@@ -0,0 +1,11 @@
+package com.example.webapp;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HelloServletTest {
+    @Test
+    public void testHelloServlet() {
+        assertEquals(2, 1 + 1);
+    }
+}
